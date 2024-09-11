2338fdc09bace8f4799facf6a019340276a415c9
swokamoto <swokamoto@gmail.com>
2024-09-11 00:02:03 -0400

    edits to SavedBooks anf SearchBooks

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -8,6 +8,9 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -60,24 +63,28 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+  const [ saveBook, { error } ] = useMutation(SAVE_BOOK);
+  
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+  
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
+  
     if (!token) {
       return false;
     }
-
+  
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
+      const { data } = await saveBook({
+        variables: { saveBook },
+      });
+  
+      if (!data) {
         throw new Error('something went wrong!');
       }
-
+  
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
